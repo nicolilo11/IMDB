ba74b866326c83a2893b8e31d424c69b519ee5a4
Nicolilo11 <nicoramitos@gmail.com>
2021-07-23 02:39:58 +0200

    feature: Actualiza claseProfesional, Crea objetos de la clase

--- claseProfesional.ts
+++ claseProfesional.ts
@@ -2,6 +2,7 @@ export class Professional
 {
     public name: string;
     public age: number;
+    public surname: string;
     public gender: string;
     public weight: number;
     public height: number;
@@ -12,42 +13,60 @@ export class Professional
     public nationality: string;
     public oscarsNumber: number;
     public profession: string;
+    public firstProject: string;
+    public lastProject: string;
+    public currentlyResidesIn : string;
+    public numberOfProductions: number;
+    public socialMedia: string;
 
-    ///////////// EL QUE LEE SE *****
-
-    constructor(name: string, age: number, gender: string,
+    constructor
+        (name: string, surname:string, age: number, gender: string,
         weight: number, height: number, hairColor: string, 
-        eyeColor: string, race: string, isRetired: boolean,
-        nationality: string, oscarsNumber: number, profession: string)
+        eyeColor: string, isRetired: boolean,nationality: string,
+        oscarsNumber: number,profession: string, firstProject: string,
+        lastProject:string, currentlyResidesIn:string, 
+        numberOfProductions: number, socialMedia
+        )
         {
             this.name = name;
+            this.surname = surname;
             this.age = age;
             this.gender = gender;
             this.weight = weight;
-            this. height = height;
+            this.height = height;
             this.hairColor = hairColor;
             this.eyeColor = eyeColor;
-            this.race = race;
             this.isRetired = isRetired;
             this.nationality = nationality;
             this.oscarsNumber = oscarsNumber;
             this.profession = profession;
+            this.firstProject = firstProject;
+            this.lastProject = lastProject;
+            this.currentlyResidesIn = currentlyResidesIn;
+            this.numberOfProductions = numberOfProductions;
+            this.socialMedia = socialMedia;
         }
 
-    valorAtributos()
+    public valorAtributos()
     {
-        let valores = "·" + this.name + "\n" + 
-                      "·" + this.age + "\n" +
-                      "·" + this.gender + "\n" +
-                      "·" + this.weight + "\n" + 
-                      "·" + this.height + "\n" + 
-                      "·" + this.hairColor + "\n" + 
-                      "·" + this.eyeColor + "\n" + 
-                      "·" + this.race + "\n" + 
-                      "·" + this.isRetired + "\n" + 
-                      "·" + this.nationality + "\n" + 
-                      "·" + this.oscarsNumber + "\n" + 
-                      "·" + this.profession + "\n" 
+        let valores = "•" + this.name + "\n" +
+                      "•" + this.surname + "\n" + 
+                      "•" + this.age + "\n" +
+                      "•" + this.gender + "\n" +
+                      "•" + this.weight + "\n" + 
+                      "•" + this.height + "\n" + 
+                      "•" + this.hairColor + "\n" + 
+                      "•" + this.eyeColor + "\n" + 
+                      "•" + this.race + "\n" + 
+                      "•" + this.isRetired + "\n" + 
+                      "•" + this.nationality + "\n" + 
+                      "•" + this.oscarsNumber + "\n" + 
+                      "•" + this.profession + "\n" +
+                      "•" + this.firstProject + "\n" +
+                      "•" + this.lastProject + "\n" +
+                      "•" + this.currentlyResidesIn + "\n" +
+                      "•" + this.numberOfProductions + "\n" 
+                      
         console.log(valores);       
     }
 }
---
+++ main.ts
@@ -0,0 +1,51 @@
+import { Professional } from "./claseProfesional";
+
+let actor1:Professional = new Professional("William Bradley", "Pitt", 57 , "Male", 80 , 1.82, "Blond", "Blue", 
+                                           false, "EE.UU", 2, "Actor - Producer", "Dallas from CBS",
+                                           "Once Upon a Time in Hollywood" , "Los Angeles - EE.UU", 60, "Doesn't have");
+
+let actor2:Professional = new Professional("Quentin Jerome ", "Tarantino", 58 , "Male", 90, 1.85 ,"Dark Brown",
+                                           "Dark Brown", false, "EE.UU", 2, "Film Director",
+                                           "Reservoir Dogs", "Once Upon a Time in Hollywood", "Ramat Aviv Gimel - Israel",
+                                           9, "@tarantinoxx");
+
+let actor3:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                            4, "@nomadlandfilm");
+
+//****** FALTAN 7 OBJETOS DE ACTORES MAS ******//
+
+let actor4:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                            4, "@nomadlandfilm");
+
+let actor5:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                            4, "@nomadlandfilm");
+let actor6:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                            4, "@nomadlandfilm");
+                                            
+let actor7:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                            4, "@nomadlandfilm");
+                                            
+let actor8:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                           4, "@nomadlandfilm");
+
+let actor9:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                           4, "@nomadlandfilm");
+
+let actor10:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                           4, "@nomadlandfilm");                                           
