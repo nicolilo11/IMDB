89e82a2469cd4429d87fcf8b98a94ca73160c72f
Nicolilo11 <nicoramitos@gmail.com>
2021-07-23 14:32:52 +0200

    feature<main.ts>: Crea 9 Objetos de la clase profesional

--- main.ts
+++ main.ts
@@ -1,51 +1,60 @@
 import { Professional } from "./claseProfesional";
 
-let actor1:Professional = new Professional("William Bradley", "Pitt", 57 , "Male", 80 , 1.82, "Blond", "Blue", 
-                                           false, "EE.UU", 2, "Actor - Producer", "Dallas from CBS",
+let bradd:Professional = new Professional("William Bradley", "Pitt", 57 , "Male", 80 , 1.82, "Blond", "Blue", 
+                                           false, "EE.UU", 2, "Film Actor - FilmProducer", "Dallas from CBS",
                                            "Once Upon a Time in Hollywood" , "Los Angeles - EE.UU", 60, "Doesn't have");
 
-let actor2:Professional = new Professional("Quentin Jerome ", "Tarantino", 58 , "Male", 90, 1.85 ,"Dark Brown",
-                                           "Dark Brown", false, "EE.UU", 2, "Film Director",
+let quentin:Professional = new Professional("Quentin Jerome ", "Tarantino", 58 , "Male", 90, 1.85 ,"Dark Brown",
+                                           "Dark Brown", false, "EE.UU", 2, "Film Director - Film Writer",
                                            "Reservoir Dogs", "Once Upon a Time in Hollywood", "Ramat Aviv Gimel - Israel",
-                                           9, "@tarantinoxx");
+                                           73, "@tarantinoxx");
 
-let actor3:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                            4, "@nomadlandfilm");
+let zhao:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
+                                        "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
+                                        "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
+                                        25, "@nomadlandfilm");
 
-//****** FALTAN 7 OBJETOS DE ACTORES MAS ******//
+let sylvester:Professional = new Professional("Sylvester Gardenzio", "Stallone ", 75 , "Male", 68, 1.77 ,"Natural Gray",
+                                            "Dark Brown", true, "EE.UU", 3, "Film Actor -  Film Producer",
+                                            "The Party at Kitty and Stud's ", "Rambo V: Last Blood ", "Los Angeles - EE.UU",
+                                            52, "@officialslystallone");
 
-let actor4:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                            4, "@nomadlandfilm");
+let actor5:Professional = new Professional("Reed ", "Morano", 44, "Male", 68, 1.70 ,"Brown",
+                                            "Dark Brown", false, "EE.UU", 0, "Film Director - Cinematographer",
+                                            "Off the Grid: Life on the Mesa", "The Rhythm Section", "New York - EE.UU",
+                                            23, "@reedmorano");
 
-let actor5:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                            4, "@nomadlandfilm");
-let actor6:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                            4, "@nomadlandfilm");
+let actor6:Professional = new Professional("Charles Stuart", "Kaufman ", 62 , "Male", 62, 1.63 ,"Brown",
+                                            "Brown", true, "EE.UU", 1, "Film Writer",
+                                            "Being John Malkovich", "I'm Thinking of Ending Things", "Pasadena - EE.UU",
+                                            17, "Www.beingcharliekaufman.com");
                                             
-let actor7:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                            "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                            "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                            4, "@nomadlandfilm");
+let actor7:Professional = new Professional("Rajmund Roman Thierry", "Polański", 87 , "Male", 62, 1.65 ,"Natural Gray",
+                                            "Dark Brown", false, "France", 1, "Film Writer - Film Director - Film Actor",
+                                            "Pokolenie", "An oficial and a Spy", "Paris - France",
+                                            42, "Doesn't have");
                                             
-let actor8:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                           4, "@nomadlandfilm");
+let actor8:Professional = new Professional("Ricardo Alberto ", "Darín ", 64, "Male", 73, 1.72 ,"Black",
+                                           "Blue", false, "Argentina", 0, "Film Actor -  Film Director",
+                                           "La Culpa", "1985", "Buenos Aires - Argentina",
+                                           91, "@ricardodarinok");
 
-let actor9:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                           4, "@nomadlandfilm");
+let actor9:Professional = new Professional("Juan José", "Campanella", 62 , "Male", 68, 1.70 ,"Bold",
+                                           "Dark Brown", false, "Argentina", 1, "Film Director -  Film Producer",
+                                           "Prioridad nacional", "Law & Order: Special Victims Unit", "Nueva York - EE.UU",
+                                           32, "www.100bares.com/campanella.htm");
 
-let actor10:Professional = new Professional("Zhao", "Ting", 39, "Female", 68, 1.70 ,"Black",
-                                           "Dark Brown", false, "China", 2, "Film Director -  Film Producer",
-                                           "Songs My Brothers Taught Me", "Nomadland", "California - EE.UU",
-                                           4, "@nomadlandfilm");                                           
+let actor10:Professional = new Professional("Seann William", "Scott", 44, "Male", 80, 1.80 ,"Black",
+                                           "Black", false, "EE.UU", 0, "Film Actor",
+                                           "Unhappily Ever After", "Already Gone", "California - EE.UU",
+                                           52, "Doesn't have");
+
+let actor11:Professional = new Professional("Juan Pablo", "Rebella", 31 , "Male", 80, 1.80 ,"Black",
+                                           "Black", true, "Uruguay", 0, "Film Director",
+                                           "Buenos y santos", "Whisky", "Montevideo - Uruguay",
+                                           5, "Doesn't have");
+
+let actor12:Professional = new Professional("Robert Anthony", "De Niro", 77, "Male", 70, 1.77 ,"Natural Gray",
+                                           "Black", false, "EE.UU", 2, "Film Actor - Film Director",
+                                           "Three Rooms in Manhattan", "Killers of the Flower Moon", "New York - EE.UU",
+                                           126, "Doesn't have");       
